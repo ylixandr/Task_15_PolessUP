17b64bc510d861e5c4d87a5d1acb968e91f1717f
Ylixandr <savoshinsky@inbox.ru>
2023-05-21 16:07:58 +0300

    Добавьте файлы проекта.

---
+++ Task_15_PolessUP/Program.cs
@@ -0,0 +1,24 @@
+﻿string str = "abaacc";
+int[] myArray = new int[str.Length];
+char letter;
+int counter = 0;
+int result = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    letter = str[i];
+    for (int j = 0; j < str.Length; j++)
+    {
+        if (letter == str[j])
+        {
+            counter++;
+        }
+        
+    }
+
+    myArray[i] = counter;
+    counter = 0;
+}
+
+result = myArray.Max() - myArray.Min();
+Console.WriteLine(result);
+
